141209c1da034f02e5fa42b9455ce02603749c46
Stefan Lehmann <Stefan.St.Lehmann@gmail.com>
2014-08-12 21:26:36 +0200

    init commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+#! python2
+from setuptools import setup
+
+setup(
+    name='lcdsonginfo',
+    version='1.0.0',
+    packages=['adafruit'],
+    url='https://github.com/MrLeeh/lcdsonginfo.git',
+    license='GPL',
+    description='',
+    requires=[],
+    scripts=['lcdsonginfo.py']
+)
